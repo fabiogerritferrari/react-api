8b0a7b7dcf063aa3160d3a7226894aefcd1fd3de
fabiogerritferrari <fabiogerritferrari@gmail.com>
2026-03-04 12:03:25 +0100

    feat: aggiunti due tatsi che permettono di cambiare la lista delle card da attrici ad attori

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,29 @@ import RenderCard from './Components/RenderCard';
 
 function App() {
   const [actors, setActors] = useState([]);
+  const [genre, setGenre] = useState('actors');
 
-  function fetchActors() {
-    axios.get('https://lanciweb.github.io/demo/api/actors/')
+  function fetchActors(genre) {
+    axios.get(`https://lanciweb.github.io/demo/api/${genre}/`)
       .then(response => setActors(response.data))
       .catch(error => console.log(error))
   }
 
-  useEffect(fetchActors, [])
+  useEffect(() => fetchActors(genre), [genre])
 
 
   return (
     <>
-      <header>
-        <h1 className='text-center my-3'>Actor's List</h1>
+      <header className='container d-flex flex-column align-items-center'>
+        <h1 className='text-center my-3'>{genre === 'actors' ? "Actor's list" : "Actresses's list"}</h1>
+        <button className='btn btn-primary my-2' onClick={() => setGenre('actors')}>Actors</button>
+        <button className='btn btn-primary my-2' onClick={() => setGenre('actresses')}>Actresses</button>
       </header>
       <main>
         <div className="container">
           <RenderCard
             actors={actors}
+            genre={genre}
           />
         </div>
       </main>
--- src/Components/RenderCard.jsx
+++ src/Components/RenderCard.jsx
@@ -1,10 +1,10 @@
-export default function ({ actors }) {
+export default function ({ actors, genre }) {
 
     return (
         <div className="row row-cols-1 row-cols-md-2 g-4 mt-4">
             {actors.map((actor) => {
                 return (
-                    <div className="col col-sm-12">
+                    <div className="col col-sm-12" key={actor.id}>
                         <div className="card mb-3">
                             <div className="row g-0">
                                 <div className="col-4">
@@ -19,8 +19,8 @@ export default function ({ actors }) {
                                         <p className="card-text">{actor.biography}</p>
                                     </div>
                                 </div>
-                                <div className="card-footer col-12">Known for: {actor.known_for.join(", ")}</div>
                             </div>
+                            <div className="card-footer col-12">Known for: {genre === "actors" ? actor.known_for : actor.most_famous_movies}</div>
                         </div>
                     </div>
                 )
